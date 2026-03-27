8223f1ba3077b1299fec30d14affca2fe38517b9
Newton <andzanimaluvana@gmail.com>
2026-03-28 01:23:22 +0200

    building welcome screen

---
+++ app/index.tsx
@@ -0,0 +1,34 @@
+import { useRouter } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView edges={["top", "bottom"]} style={{ padding: 10 }}>
+      <View>
+        <Text>Hello welcome</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
---
+++ app/welcome_screen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const welcome_screen = () => {
+  return (
+    <View>
+      <Text>welcome_screen</Text>
+    </View>
+  );
+};
+
+export default welcome_screen;
